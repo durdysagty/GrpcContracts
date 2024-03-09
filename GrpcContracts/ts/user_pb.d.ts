f46329d1d37fc9906201af441e6d04001d4faa5d
durdysagty <durdysagty@gmail.com>
2024-03-09 23:38:59 +0300

    Add project files.

---
+++ GrpcContracts/ts/user_pb.d.ts
@@ -0,0 +1,25 @@
+// package: GrpcContracts
+// file: user.proto
+
+import * as jspb from "google-protobuf";
+
+export class User extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): User.AsObject;
+  static toObject(includeInstance: boolean, msg: User): User.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: User, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): User;
+  static deserializeBinaryFromReader(message: User, reader: jspb.BinaryReader): User;
+}
+
+export namespace User {
+  export type AsObject = {
+    name: string,
+  }
+}
+
